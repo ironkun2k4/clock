19d896f171fc15ebf3abca79273a35d385343c13
Jacky Lee <jla524@sfu.ca>
2022-03-10 18:57:55 -0800

    Add dropdown with dummy data

--- volunteer_hours/app.py
+++ volunteer_hours/app.py
@@ -3,8 +3,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+events = ['2022 event 1', '2022 event 2', '2022 event 3']
+
 
 @app.route('/')
 def welcome():
-    content = render_template('index.html')
+    content = render_template('index.html', events=events)
     return content
