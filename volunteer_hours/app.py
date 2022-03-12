06520f0f43944c341bcc375b0cabdb14767fbe8c
Jacky Lee <jla524@sfu.ca>
2022-03-12 10:13:20 -0800

    Use find_dotenv to locate file

--- volunteer_hours/app.py
+++ volunteer_hours/app.py
@@ -8,7 +8,7 @@ events = ['2022 event 1', '2022 event 2', '2022 event 3']
 @app.route('/', methods=['POST', 'GET'])
 def welcome():
     if request.method == 'POST':
-        # TODO: Use request.form to access form input values
+        # Use request.form to access form input values
         ...
     else:
         content = render_template('index.html', events=events)
