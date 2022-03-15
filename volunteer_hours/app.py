1c553a192b093c55efafde5df2553b46f380565f
Jacky Lee <jla524@sfu.ca>
2022-03-15 16:43:45 -0700

    Event finder works

--- volunteer_hours/app.py
+++ volunteer_hours/app.py
@@ -7,10 +7,21 @@ from volunteer_hours.api.ragic import Ragic
 app = Flask(__name__)
 
 
-def get_events(member_id) -> list[str]:
-    events = Ragic().fetch_events(member_id)
-    names = [event['Opportunity'] for event in events.values()]
-    return names
+class Member:
+    def __init__(self):
+        self._member_id = ''
+
+    def set_member_id(self, member_id) -> None:
+        if member_id.startswith('LYN'):
+            self._member_id = member_id
+
+    def get_events(self) -> list[str]:
+        events = Ragic().fetch_events(self._member_id)
+        names = [event['Opportunity'] for event in events.values()]
+        return names
+
+
+member = Member()
 
 
 @app.route('/')
@@ -25,11 +36,12 @@ def main_screen():
 @app.route('/action', methods=['GET', 'POST'])
 def action_screen():
     if request.method == 'POST':
-        print(request.form, file=sys.stdout)
         member_id = request.form.get('member_id')
-        events = get_events(member_id)
-        response = make_response(f"{{'response': {events}}}")
+        member.set_member_id(member_id)
+        response = make_response("{{'response': {member_id}}")
         response.headers = {'Content-Type': 'application/json'}
         return response
-    content = render_template('action.html', events=events)
-    return content
+    else:
+        events_list = member.get_events()
+        content = render_template('action.html', events=events_list)
+        return content
